bf932f5fa8eee7949d3e5224db3b268e61deea81
kmicic77 <kmicic77@gmail.com>
2016-08-17 13:11:31 +0100

    Comments
    Add comments, remove print(a), start drawing figures on left side of the
    screen instead of the centre

--- ex4-3.py
+++ ex4-3.py
@@ -1,13 +1,14 @@
 import turtle
 import math
-
+""" draw 3 'sliced' polygons using general functions"""
 def triangle (t,r,angle):
+    #r - leg of isosceles triangle
     t.fd(r)
     triangle_angle=((180-angle)/2)
 	
     t.lt(180-triangle_angle)
+#a - base of isosceles triangle
     a=math.sqrt(2*r**2*(1-math.cos(angle*math.pi/180)))
-    print (a)
     t.fd(a)
     t.lt(180-triangle_angle)
     t.fd(r)
@@ -21,6 +22,7 @@ def move (t,length):
 		t.pd()
 		
 bob=turtle.Turtle()
+move (bob,-200)
 figure (bob,5,100)
 move(bob,220)
 figure (bob,6,100)
